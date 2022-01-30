5c286f003e40cc374ec8239bbe4b67550d679c2d
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2022-01-30 09:09:21 +0000

    managed to make selenium work

--- poc_selenium.py
+++ poc_selenium.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-# path="C:\\Program Files\\geckodriver\\geckodriver.exe"
-path="C:\\Python\\geckodriver\\geckodriver.exe"
 
-driver=webdriver.Firefox(path)
 
-driver = webdriver.Firefox()
+driver=webdriver.Firefox(executable_path="C:\Python\geckodriver\geckodriver.exe")
 driver.get("http://www.python.org")
+
 assert "Python" in driver.title
+
 elem = driver.find_element_by_name("q")
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
+
 assert "No results found." not in driver.page_source
+
 driver.close()
