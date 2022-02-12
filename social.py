e6d981526b9ad4d8e9b0b07e48268b02e6d03747
victorf <mail@victorf.ch>
2022-02-12 11:29:33 +0000

    moved socialmedia class to its own file

---
+++ social.py
@@ -0,0 +1,30 @@
+import yaml, tweepy
+
+class SocialMedia:    
+    def __init__(self):
+        self.auth = None        
+        self.api = None
+        self.set_auth()
+        self.set_api()
+        
+        
+    def set_auth(self):
+        with open('config.yml') as f:    
+            config_vars = yaml.safe_load(f)
+            
+        api_key = config_vars['api_key']
+        api_secrets = config_vars['api_key_secret']
+        access_token = config_vars['access_token']
+        access_secret = config_vars['access_token_secret']
+        
+        self.auth = tweepy.OAuthHandler(api_key, api_secrets)
+        self.auth.set_access_token(access_token, access_secret) 
+        
+    
+    def set_api(self):        
+        self.api = tweepy.API(self.auth)
+        
+    
+    def tweet(self, status):    
+        tweet = self.api.update_status(status=status, card_uri='tombstone://card')  
+        print(f'Tweeted about it @ {tweet.created_at}')
--- termooobot.py
+++ termooobot.py
@@ -5,6 +5,7 @@ sys.path.append("..")
 from datetime import datetime
 from selenium import webdriver
 from unidecode import unidecode
+from social import SocialMedia
 
 
 
@@ -48,37 +49,6 @@ class Guess:
             last_mentioned_on = self.word.last_mentioned_on.strftime("%Y-%m-%d")
             
         print(f'[Last mentioned on: {last_mentioned_on}]')
-        
-
-
-class SocialMedia:    
-    def __init__(self):
-        self.auth = None        
-        self.api = None
-        self.set_auth()
-        self.set_api()
-        
-        
-    def set_auth(self):
-        with open('config.yml') as f:    
-            config_vars = yaml.safe_load(f)
-            
-        api_key = config_vars['api_key']
-        api_secrets = config_vars['api_key_secret']
-        access_token = config_vars['access_token']
-        access_secret = config_vars['access_token_secret']
-        
-        self.auth = tweepy.OAuthHandler(api_key, api_secrets)
-        self.auth.set_access_token(access_token, access_secret) 
-        
-    
-    def set_api(self):        
-        self.api = tweepy.API(self.auth)
-        
-    
-    def tweet(self, status):    
-        tweet = self.api.update_status(status=status, card_uri='tombstone://card')  
-        print(f'Tweeted about it @ {tweet.created_at}')
         
         
         
